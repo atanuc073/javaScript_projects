d019979c6eb5c9eab09e453ae352e87c2f8e6459
atanuc073 <atanuc073@gmail.com>
2021-06-08 12:37:58 +0530

    finished model window project

--- Modal_window/starter/script.js
+++ Modal_window/starter/script.js
@@ -3,7 +3,25 @@ const modal=document.querySelector(".modal");
 const overlay=document.querySelector(".overlay");
 const btnCloseModal=document.querySelector(".close-modal");
 const btnOpenModal=document.querySelectorAll(".show-modal");
-console.log(btnCloseModal);
-// for(let i=0; i < btnOpenModal.length;i++)
-//     console.log(btnOpenModal[i].textContent);
-btnOpenModal.forEach(element => {console.log(element.textContent)});
+
+const openModal=function(){
+    modal.classList.remove("hidden");
+    overlay.classList.remove('hidden');
+};
+
+const closeModal =function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+btnOpenModal.forEach(element => {element.addEventListener("click",openModal)});
+btnCloseModal.addEventListener("click",closeModal);
+overlay.addEventListener("click",closeModal);
+
+// when key is pressed
+document.addEventListener("keydown",function(e){
+    if(e.key=="Escape"){
+        if(!modal.classList.contains('hidden'))
+            closeModal();
+    }
+})
