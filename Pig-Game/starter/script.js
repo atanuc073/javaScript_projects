010b2b3d5de6d923d247673f74906adfc64e8b30
atanuc073 <atanuc073@gmail.com>
2021-06-10 14:03:39 +0530

    storing the scores inarray for players

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 // Selecting Elements
+const player0El=document.querySelector(".player--0");
+const player1El=document.querySelector(".player--1");
 const score0El=document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const current0El= document.getElementById("current--0");
@@ -9,9 +11,10 @@ const diceNew=document.querySelector(".btn--new");
 const diceRoll=document.querySelector(".btn--roll");
 const diceHold=document.querySelector(".btn--hold");
 
+
 let currentScore=0;
 let activeplayer=0;
-const scoreArr=[0,0];
+let scoreArr=[0,0];
 //Initial Conditions
 score0El.textContent=0;
 score1El.textContent=0;
@@ -34,10 +37,29 @@ diceRoll.addEventListener("click",function(){
 
 
     }else{
-        currentScore=0
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
+        currentScore=0;
+        document.getElementById(`current--${activeplayer}`).textContent=0;
         activeplayer= activeplayer === 0 ? 1 : 0;
+        
 
     }
 
 
+})
+
+diceHold.addEventListener("click",function(){
+    scoreArr[activeplayer]+=currentScore;
+    currentScore=0;
+    if(scoreArr[activeplayer]<100){
+        document.querySelector(`#score--${activeplayer}`).textContent=scoreArr[activeplayer];
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
+        document.getElementById(`current--${activeplayer}`).textContent=0;
+        activeplayer= activeplayer === 0 ? 1 : 0;
+    }
+    else{
+        
+    }
 })
